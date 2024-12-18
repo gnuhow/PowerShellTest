1c43e5be02dda2e6628e017636e5077b21ea3639
gnuhow <gnuhow@gmail.com>
2024-12-18 10:09:19 -0500

    small changes to git commands

---
+++ git-commands.sh
@@ -0,0 +1,13 @@
+# clone the branch
+git clone https://github.com/gnuhow/PowerShellTest.git
+
+# make a new branch and push it
+git checkout -b mybranch
+git push -u origin mybranch
+
+# add files to the commit, make the commit and push changes
+git add .
+git commit -m "Initial commit"
+git push
+
+
