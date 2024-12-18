e442e7cf23dbd47a1641efef53bea91311632389
gnuhow <gnuhow@gmail.com>
2024-12-18 10:40:12 -0500

    rebase feature branch onto main

--- example.ps1
+++ example.ps1
@@ -8,12 +8,20 @@ function example {
     Import-Module ActiveDirectory
     . .\import.ps1
 
+    $fileName = "test.txt"
+    $folderName = "newFolder"
+
     Write-Host -ForegroundColor Yellow "Global var: `"$GLOBAL_VAR`""
     Write-Host -ForegroundColor Green "User Name: $name" 
-    Write-Output "file data" | Out-File -FilePath test.txt
-    Write-Output "more data" | Add-content -Path test.txt
+    Write-Output "file data" | Out-File -FilePath $fileName
+    Write-Output "more data" | Add-content -Path $fileName
 
-    New-Item -Path "newFolder"
+    New-Item -Path $folderName
+    Get-ChildItem $folderName
+
+    Remove-Item $fileName
+    Remove-Item $folderName
 }
 
 example -name "charlie" -age 33 -isAlive 1
+
