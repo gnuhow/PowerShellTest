dd6b80e2c53614a5fb729f2f4fb693617616fdb9
gnuhow <gnuhow@gmail.com>
2024-12-18 12:53:45 -0500

    more powershell syntax examples

--- example.ps1
+++ example.ps1
@@ -1,3 +1,14 @@
+Import-Module PSReadLine
+
+# import a powershell script into the environment
+. .\import.ps1
+
+$filename = "test.txt"
+$folderName = "newFolder"
+$jsonFilename = "example.json"
+$loremString = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+
+# example function
 function example {
     param(
         [string] $name,
@@ -5,24 +16,71 @@ function example {
         [bool] $isAlive
     )
 
-    Import-Module ActiveDirectory
-    . .\import.ps1
-
-    # yay code!
-    $fileName = "test.txt"
-    $folderName = "newFolder"
-
-    Write-Host -ForegroundColor Yellow "Global var: `"$GLOBAL_VAR`""
     Write-Host -ForegroundColor Green "User Name: $name" 
-    Write-Output "file data" | Out-File -FilePath $fileName
-    Write-Output "more data" | Add-content -Path $fileName
-
-    New-Item -Path $folderName
-    Get-ChildItem $folderName
-
-    Remove-Item $fileName
-    Remove-Item $folderName
 }
+example -name "charlie" -age 33 -isAlive $true
 
-example -name "charlie" -age 33 -isAlive 1
+
+# Write host, pring objects to the screen
+Write-Host -ForegroundColor Yellow "Global var: `"$GLOBAL_VAR`""
+
+#  Write-output objects to a pipeline
+Write-Output "file data" | Out-File -FilePath $filename
+Write-Output "more data" | Add-content -Path $filename
+
+# make a new folder
+New-Item -Path $folderName -ItemType Directory
+
+# ls the new folder
+Get-ChildItem $folderName
+
+# load Json into an object
+$exampleHashTable = Get-Content -Raw "$jsonFilename" | ConvertFrom-Json 
+
+# view the contents of an object
+Select-Object -InputObject $exampleHashTable -Property *
+Out-String -InputObject $exampleHashTable
+
+# get values from an hashtable
+$item2 = Select-Object -InputObject $exampleHashTable -Property "object" -ExpandProperty "object" | Select-Object -Property "item2" | select-object -Property *
+Write-Host "Selected item2: $item2"
+
+# object dot notation
+$value = $exampleHashTable.object.item2
+Write-Host "Dot notation item2: $value"
+
+# PSItem to reference the current pipeline object
+Get-Process | Where-Object {$PSItem.ProcessName -imatch "winlogon"}
+
+# do text parsing
+Select-String -Path loremIpsum.txt -Pattern '.*maximus.*' -CaseSensitive
+
+# String arrays
+$loremCharArray = $loremString.ToCharArray()
+Write-Host "first char $($loremCharArray[0])"          # l
+Write-Host "first word $($loremCharArray[0..4])"  # lorem
+Write-Host "last char $($loremCharArray[-1])"          # last character "."
+$loremCharArray[6] = "+"
+
+# String array looping and manipulation
+# $loremCharArray = $loremString.ToCharArray()
+$resultCharArray = $loremCharArray
+$position = 0
+foreach ($character in $loremCharArray) {
+    if ($character -ceq "l") {                              # case sensitive equals
+        Write-Host "position: $position, character: $character"
+        $resultCharArray[$position] = "w"
+    } elseif ($character -ceq "L") {
+        $resultCharArray[$position] = "W"
+    }
+    $position++
+}
+$resultString = $resultCharArray -join ''                  # join the array back into a string
+Write-Host "result: $resultString"
+
+
+# cleanup
+Remove-Item $filename
+Remove-Item $folderName
+
 
--- gitCommands.sh
+++ gitCommands.sh
@@ -1,14 +1,17 @@
 # clone the branch
 git clone https://github.com/gnuhow/PowerShellTest.git
 
+
 # make a new branch and push it
 git checkout -b mybranch
 git push -u origin mybranch
+
 
 # add files to the commit, make the commit and push changes
 git add .
 git commit -m "Initial commit"
 git push
+
 
 # update a feature branch from main
 git add .
